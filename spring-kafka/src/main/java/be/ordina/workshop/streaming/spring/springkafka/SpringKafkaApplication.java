6724a0d9edfd023213af2bff811572128b2f3632
Tom Van den Bulck <Tom.vandenBulck@partner.alphabet.com>
2018-06-21 08:38:26 +0200

    Added base projects for spring kafka and apache storm

---
+++ spring-kafka/src/main/java/be/ordina/workshop/streaming/spring/springkafka/SpringKafkaApplication.java
@@ -0,0 +1,12 @@
+package be.ordina.workshop.streaming.spring.springkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaApplication.class, args);
+	}
+}
---
+++ storm/src/main/java/be/ordina/workshop/streaming/storm/storm/StormApplication.java
@@ -0,0 +1,12 @@
+package be.ordina.workshop.streaming.storm.storm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StormApplication.class, args);
+	}
+}
