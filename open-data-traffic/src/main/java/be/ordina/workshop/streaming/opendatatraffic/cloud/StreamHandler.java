d0599758a2260be7df625ae0a5cf44ec17b69d99
Tom Van den Bulck <tom.vandenbulck@ordina.be>
2018-08-09 23:23:21 +0200

    cleaned native java processing

--- open-data-traffic/src/main/java/be/ordina/workshop/streaming/opendatatraffic/cloud/StreamHandler.java
+++ open-data-traffic/src/main/java/be/ordina/workshop/streaming/opendatatraffic/cloud/StreamHandler.java
@@ -21,8 +21,6 @@ public class StreamHandler {
 
     private final HashMap<String, Integer> vehicleCount;
 
-
-
     private final Channels inputChannels;
 
     private MessageHandler messageHandler;
@@ -52,10 +50,7 @@ public class StreamHandler {
             log.info(" the sensor id is " + event.getSensorId());
 
             if (event.getTrafficIntensity() > 0) {
-                //log.info("We now have {} vehicles on the road {} for lane:{}", event.getTrafficIntensity(), event.getSensorData().getName(), event.getSensorData().getTrafficLane());
-
                 log.info("We now have {} vehicles on the road {}", event.getTrafficIntensity(), event.getSensorId());
-
 
                 int vehicleCountForEvent = event.getTrafficIntensity();
 
@@ -63,32 +58,22 @@ public class StreamHandler {
                     vehicleCountForEvent += vehicleCount.get(event.getSensorId());
                 }
 
-                //log.info("We now had total: {} vehicles on road: {} for lane: {}", vehicleCountForEvent, event.getSensorData().getName(), event.getSensorData().getTrafficLane());
                 log.info("We now had total: {} vehicles", vehicleCountForEvent);
-
 
                 vehicleCount.put(event.getSensorId(), vehicleCountForEvent);
             }
 
-
             if (event.getVehicleSpeedCalculated() > 0) {
                 if (lowestWithTraffic.get(event.getSensorId()) == null || lowestWithTraffic.get(event.getSensorId()).getVehicleSpeedCalculated() > event.getVehicleSpeedCalculated()) {
                     lowestWithTraffic.put(event.getSensorId(), event);
-
-                    //log.info("Updated lowestWithTraffic for sensor {} with an event with speed {} for vehicle {} ", event.getSensorData().getName() + " " + event.getSensorData().getTrafficLane()
-                    //        , event.getVehicleSpeedCalculated(), event.getVehicleClass().name());
                 }
 
                 if (highestWithTraffic.get(event.getSensorId()) == null || highestWithTraffic.get(event.getSensorId()).getVehicleSpeedCalculated() < event.getVehicleSpeedCalculated()) {
                     highestWithTraffic.put(event.getSensorId(), event);
-
-                    //log.info("Updated highestTraffic for sensor {} with an event with speed {} for vehicle {} ", event.getSensorData().getName() + " " + event.getSensorData().getTrafficLane()
-                    //        , event.getVehicleSpeedCalculated(), event.getVehicleClass().name());
                 }
 
                 messages.add(event);
             }
-
         });
 
         inputChannels.trafficEvents().subscribe(messageHandler);
@@ -108,7 +93,6 @@ public class StreamHandler {
     }
 
     public List<TrafficEvent> getMessages() {
-
         List<TrafficEvent> messagesToReturn = new ArrayList<>();
 
         if (messageHandler != null) {
@@ -118,7 +102,6 @@ public class StreamHandler {
         }
 
         this.subScribeOnChannel();
-
 
         return messagesToReturn;
     }
